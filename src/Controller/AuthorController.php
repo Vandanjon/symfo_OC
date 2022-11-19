ce69057a60f28687049b0363e01a395b17c46ffa
Benoît V <82836739+Vandanjon@users.noreply.github.com>
2022-11-19 19:06:18 +0100

    Add files via upload

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Author;
+use App\Repository\AuthorRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AuthorController extends AbstractController
+{
+    #[Route('/api/authors', name: 'authors', methods: ['GET'])]
+    public function getAllAuthor(AuthorRepository $authorRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $authorList = $authorRepository->findAll();
+
+        $jsonAuthorList = $serializer->serialize($authorList, 'json', ['groups' => 'getAuthors']);
+
+        return new JsonResponse($jsonAuthorList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/authors/{id}', name: 'detailAuthor', methods: ['GET'])]
+    public function getAuthor(Author $author, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonAuthor = $serializer->serialize($author, 'json', ['groups' => 'getAuthors']);
+
+        return new JsonResponse($jsonAuthor, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/authors/{id}', name: 'deleteAuthor', methods: ['DELETE'])]
+    public function deleteAuthor(Author $author, EntityManagerInterface $em):JsonResponse
+    {
+        $em->remove($author);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+}
---
+++ src/Controller/BookController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BookController extends AbstractController
+{
+    #[Route('/api/books', name: 'books', methods:['GET'])]
+    public function getAllBooks(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $bookList = $bookRepository->findAll();
+
+        $jsonBookList = $serializer->serialize($bookList, 'json', ['groups' => 'getBooks']);
+
+        return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/books/{id}', name: 'detailBook', methods: ['GET'])]
+    public function getDetailBook(Book $book, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonBook = $serializer->serialize($book, 'json', ['groups' => 'getBooks']);
+    
+        return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/books/{id}', name: 'deleteBook', methods: ['DELETE'])]
+    public function deleteBook(Book $book, EntityManagerInterface $em): JsonResponse
+    {
+        $em->remove($book);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+}
