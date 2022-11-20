11953f55281d9bf442ced2dc789d43931207369b
Splagadou <benoit@vandanjon.fr>
2022-11-20 15:10:45 +0100

    test push starting symfony

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -26,6 +26,7 @@ class Book
 
     #[ORM\ManyToOne(targetEntity: Author::class, inversedBy: 'books')]
     #[Groups(["getBooks"])]
+    #[ORM\JoinColumn(onDelete:"CASCADE")]
     private ?Author $author = null;
 
     public function getId(): ?int
